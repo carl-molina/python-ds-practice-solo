2edaa9d2502df6dfe045f23ef079d89000fce477
Carl Molina <carlandrew.molina@gmail.com>
2023-11-27 16:56:35 -0800

    Working on Problem 11

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -12,3 +12,34 @@ def flip_case(phrase, to_swap):
 
     """
 
+
+
+
+
+
+
+    # return "".join(char.swapcase() for char in phrase if char.lower() == to_swap.lower())
+
+    # phrase_list = list(phrase)
+
+    # return "".join([char.swapcase() for char in phrase_list if char.lower() == to_swap.lower()])
+
+    # print('Before the for loop')
+    # for char in phrase_list:
+    #     print('entering for loop')
+    #     to_swap = to_swap.lower()
+    #     print('this is to_swap', to_swap)
+    #     lower_case_char = char.lower()
+    #     print('this is lower_case_char', lower_case_char)
+    #     if to_swap == lower_case_char:
+    #         print('to_swap and lower_case are the same')
+    #         char = char.swapcase()
+
+    #         if (char == char.lower()):
+    #             print("if char == char.lower() makes it here")
+    #             char = char.upper()
+    #             print('This is char', char)
+    #         else:
+    #             print("gets to the else statement instead")
+    #             char = char.lower()
+    # return "".join(phrase_list)
